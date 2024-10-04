1e1acc7d343878bb6f8c1a7b869b70522dd900bf
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-04 17:30:12 +0530

    Static and Dynamic Rendering npm-build npm-start

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,11 +6,13 @@ name: string,
 }
 
 const UserPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users")
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", { cache: 'no-store'}) 
+  // { next: {revalidate: 10}} cache function is only implemented in fetch function
   const users: User[] = await res.json()
   return (
     <>
     <h1>Users</h1>
+    <p>{new Date().toLocaleTimeString()}</p> 
     <ul>
       {users.map(user => <li key={user.id}>{user.name}</li>)}
     </ul>
