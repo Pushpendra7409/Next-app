b7d3f0a79b05b4bc7ea34f819751c3a53e3e8dc7
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-05 06:59:53 +0530

    Dynamic routing

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,34 +1,14 @@
 import React from 'react'
+import UserTable from './UserTable'
 
-interface User  {
-id: number,
-name: string,
-email: string,
-}
 
-const UserPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", { cache: 'no-store'}) 
-  // { next: {revalidate: 10}} cache function is only implemented in fetch function
-  const users: User[] = await res.json()
+
+const UserPage = () => {
   return (
     <>
     <h1>Users</h1>
     {/* <p>{new Date().toLocaleTimeString()}</p>  */}
-    <table className='table table-bordered'>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-        </tr>
-      </thead>
-      <tbody>
-      {users.map(user => <tr key={user.id}>
-      <td>{user.name}</td>
-      <td>{user.email}</td>
-      </tr>)}
-      </tbody>
-      
-    </table>
+    <UserTable />
     </>
   )
 }
