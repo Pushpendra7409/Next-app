e7a0a36a4251600c44b2cc265a9d0cd93382aa7f
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-05 15:52:34 +0530

    Updating an object /api/users/1 on POSTMAN PUT request

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,3 +1,4 @@
+import { request } from "http";
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(
@@ -10,3 +11,20 @@ export function GET(
 
  return NextResponse.json({ id: 1, name: 'John' });
     }
+
+    export async function PUT(
+      request: NextRequest,
+        { params }: {params: { id: number }}) {
+         //validate the request body
+         const body = await request.json();
+         if(!body.name)
+            return NextResponse.json({error: 'Name is required'}, {status: 400})
+         //If valid return 400 
+         //Fetch the user with given id
+         //If does'nt exist, return 404 error 
+         if (params.id > 10)
+            return NextResponse.json({ error: 'User Not found' }, { status: 404});
+         //updated user
+         //Return the updated user
+         return NextResponse.json({ id: 1, name: body.name }, { status: 200 });
+        }
