9e639d41f5921fdb9108636bddb8c1b53f048b9c
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-05 07:31:20 +0530

    Accessing query string parameters npm fast sort

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import UserTable from './UserTable'
 
+interface Props {
+  searchParams: { sortOrder: string}
+}
 
-
-const UserPage = () => {
+const UserPage = async ({searchParams: {sortOrder}}: Props) => {
   return (
     <>
     <h1>Users</h1>
     {/* <p>{new Date().toLocaleTimeString()}</p>  */}
-    <UserTable />
+    <UserTable sortOrder={sortOrder}/>
     </>
   )
 }
