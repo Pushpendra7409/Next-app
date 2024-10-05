0f1de5ef62c24e37afaff4178556fbf3eefca352
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-05 11:53:49 +0530

    Creating an Object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -7,3 +7,12 @@ export function GET(request: NextRequest) {
         { id: 1, name: "Doe", email: "doe@gmail.com" },
     ])
 }
+
+export async function POST(request: NextRequest) {
+    const body =  await request.json()
+    //validate 
+    //If invalid, return 400 else , return error
+    if(!body.name)
+        return NextResponse.json({ error: 'Name is required'}, { status: 400})
+    return NextResponse.json({id: 1, name: body.name}, { status: 201 });
+}
