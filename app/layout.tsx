c3564a7a09c6ef4b210ea24bbd18c6920de250c4
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-05 08:11:16 +0530

    Layout and Navbar in root component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import NavBar from "./NavBar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter = Inter({
+  subsets: ["latin"],  // Load the required subsets
 });
 
 export const metadata: Metadata = {
@@ -25,11 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" data-theme="winter">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={inter.className}>
+        <NavBar />
+        <main className="p-5">{children}</main>
+        </body>
     </html>
   );
 }
