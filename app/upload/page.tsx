2b265e6e4edd11d850c3434422be4a71c061e9ad
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-06 14:02:16 +0530

    Uploading files

---
+++ app/upload/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+import React from 'react'
+import { CldUploadWidget } from 'next-cloudinary';
+
+const UploadPage = () => {
+  return (
+    <CldUploadWidget uploadPreset='ya6leezm'>
+      {({ open }) => <button 
+      className='btn btn-primary'
+      onClick={() => open()}>Upload</button>}
+    </CldUploadWidget>
+  )
+}
+
+export default UploadPage
