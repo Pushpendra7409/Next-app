2af52790049056115c43553672c2f11399b504db
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-06 16:16:51 +0530

    Customized cloudinary

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -15,6 +15,29 @@ const UploadPage = () => {
     }
     <CldUploadWidget 
     uploadPreset='ya6leezm'
+    options={{
+      sources: ['local'],
+      multiple: false,
+      maxFiles: 5,
+      styles: {
+        palette: {
+            window: "#FFFFFF",
+            windowBorder: "#90A0B3",
+            tabIcon: "#0078FF",
+            menuIcons: "#5A616A",
+            textDark: "#000000",
+            textLight: "#FFFFFF",
+            link: "#0078FF",
+            action: "#FF620C",
+            inactiveTabIcon: "#0E2F5A",
+            error: "#F44235",
+            inProgress: "#0078FF",
+            complete: "#20B832",
+            sourceBg: "#E4EBF1"
+       
+         }  
+        },
+    }}
     onSuccess= {(result, widget) => {
       if (result.event !== 'success') return 
       const info = result.info as CloudinaryResult
